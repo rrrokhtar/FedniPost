33f6377c1a3cba73d5a95f7fd84df5ce21a4348b
Mohamed Mokthar <mohamed.mahmoud98@eng-st.cu.edu.eg>
2022-12-04 22:27:49 +0200

    Updated app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { PostService } from './post/post.service';
+import { DBService } from './services/db.service';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ScrollingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, PostService],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, DBService],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
